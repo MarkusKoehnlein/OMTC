42da39db227f34e4dbb226f26a0ebe6a17552584
MarkusKoehnlein <108465772+MarkusKoehnlein@users.noreply.github.com>
2022-07-01 14:28:25 +0200

    Update Coding.R

--- R/Coding.R
+++ R/Coding.R
@@ -4,14 +4,14 @@ Coding = function(Daten){
   metric_old = lm(y ~ x, data = Daten)
   means <- aggregate(Daten$y, list(Daten$x), FUN = mean)
   names(means)[names(means)=="x"] <- "y_means"
-  names(means)[names(means)=="Group.1"] <- "x_oc"
-  means <- means[order(means$x_oc),]
+  names(means)[names(means)=="Group.1"] <- "x"
+  means <- means[order(means$x),]
   means$k = order(seq(1, length(unique(Daten$x)),1))
   means$x_nc = (means$y_means - coef(metric_old)[1]) / coef(metric_old)[2]
-  means <- means[c("k", "x_oc", "x_nc", "y_means")]
-  Daten$y_means = ifelse(Daten$x == means$x_oc, means$y_means)
-  Daten$x_nc = (Daten$y_means - coef(metric_old)[1]) / coef(metric_old)[2]
+  means <- means[c("k", "x", "x_nc", "y_means")]
+  Daten = merge(Daten, means, by = "x")
+  Daten$k = NULL
+  means <- rename(means, c(x = "x_oc"))
   Daten$y_means = NULL
   return(Daten)
 }
-
